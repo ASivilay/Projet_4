a3009e713df0bf5c6dba516eb3c5e508ffb22256
Alric <acsivilay@gmail.com>
2023-01-21 11:26:39 +0100

    modal clean

--- scripts/script5.js
+++ scripts/script5.js
@@ -108,37 +108,30 @@ async function getModal(element){
 ///////////////////////////////////////////////////////////////////////////////
 function initModal(){
     // Get the modal
-    let modal = document.getElementById("myModal");
-    console.log(modal);
-
+    let movieModal = document.getElementById("movieModal");
+    
     // Get the button that opens the modal
-    let btn = document.getElementById("myBtn");
-    console.log(btn);
+    //let btn = document.getElementById("myBtn");
+    //console.log(btn);
 
     // Get the <span> element that closes the modal
     let span = document.getElementsByClassName("close")[0];
 
     // When the user clicks on the button, open the modal
-    btn.onclick = function() {
-        modal.style.display = "block";
-    }
+    //btn.onclick = function() {
+    //    modal.style.display = "block";
+    //}
 
     // When the user clicks on <span> (x), close the modal
     span.onclick = function() {
-        modal.style.display = "none";
+        movieModal.style.display = "none";
     }
 
     // When the user clicks anywhere outside of the modal, close it
     window.onclick = function(event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
+        if (event.target == movieModal) {
+            movieModal.style.display = "none";
         }
-    }
-
-    let movieModal = document.getElementById("movieModal");
-    let span2 = document.getElementsByClassName("close")[1];
-    span2.onclick = function(){
-        movieModal.style.display = "none";
     }
 }
 
