bde5c93905f2e12f1c65b32d88ef6ce7860ddb50
Alric SIVILAY <alric.sivilay@infotel.com>
2023-02-20 17:06:38 +0100

    deleted old files

--- scripts/script6.js
+++
@@ -1,9 +0,0 @@
-//url = "http://localhost:8000/api/v1/titles/?page_size=1&year=&min_year=&max_year=&imdb_score=&imdb_score_min=&imdb_score_max=&title=&title_contains=&genre=&genre_contains=&sort_by=-imdb_score&director=&director_contains=&writer=&writer_contains=&actor=&actor_contains=&country=&country_contains=&lang=&lang_contains=&company=&company_contains=&rating=&rating_contains="
-url = "http://localhost:8000/api/v1/titles/?page_size=7&year=&min_year=&max_year=&imdb_score=&imdb_score_min=&imdb_score_max=&title=&title_contains=&genre=&genre_contains=&sort_by=-imdb_score&director=&director_contains=&writer=&writer_contains=&actor=&actor_contains=&country=&country_contains=&lang=&lang_contains=&company=&company_contains=&rating=&rating_contains="
-
-const myRequest = new Request(url);
-fetch(myRequest)
-.then((response) => response.json())
-.then((data) => {
-    for (const )
-})
